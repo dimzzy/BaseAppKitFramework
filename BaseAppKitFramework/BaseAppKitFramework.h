5a2ccd6d3f221ef29c3f8b7acc2c09e41211cd77
Dmitry Stadnik <dimzzy@gmail.com>
2011-12-28 15:17:52 +0100

    Initial Commit

---
+++ BaseAppKitFramework/BaseAppKitFramework.h
@@ -0,0 +1,13 @@
+//
+//  BaseAppKitFramework.h
+//  BaseAppKitFramework
+//
+//  Created by Stadnik Dmitry on 12/28/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BaseAppKitFramework : NSObject
+
+@end
---
+++ BaseAppKitFramework/BaseAppKitFramework.m
@@ -0,0 +1,23 @@
+//
+//  BaseAppKitFramework.m
+//  BaseAppKitFramework
+//
+//  Created by Stadnik Dmitry on 12/28/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "BaseAppKitFramework.h"
+
+@implementation BaseAppKitFramework
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
