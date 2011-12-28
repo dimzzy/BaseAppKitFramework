2c460e399185aba77561ac69a8e83b73b3d56a68
Dmitry Stadnik <dimzzy@gmail.com>
2011-12-28 19:38:53 +0100

    Fix symlinks

--- build.sh
+++ build.sh
@@ -13,4 +13,5 @@ mkdir -p $OUTPUT
 /Developer/usr/bin/xcodebuild -target BaseAppKit -configuration Release build || failed build;
 
 cd $WORKSPACE/build/Release-iphoneuniversal || failed output;
-zip -r $OUTPUT/BaseAppKit.framework.zip BaseAppKit.framework
+rm BaseAppKit
+zip -r -y -9 $OUTPUT/BaseAppKit.framework.zip BaseAppKit.framework
